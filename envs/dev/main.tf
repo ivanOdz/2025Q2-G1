694a889c06d2a9787ce2b6da8926283c2713aecb
Diego-Rabinovich <120027298+Diego-Rabinovich@users.noreply.github.com>
2025-10-20 21:12:09 -0300

    modularized providers

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,16 +1,4 @@
 # Orquestación: invoca módulos locales y externos
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,1 +1,13 @@
 # Config del/los provider(s) y default_tags
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
