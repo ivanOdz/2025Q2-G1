7dcc32aa4c9c00936ea7a402d2c9ce2e03110478
diegobadin <dbadin@itba.edu.ar>
2025-10-20 22:22:35 -0300

    DynamoDB module moved to backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -102,3 +102,58 @@ module "frontend_bucket" {
     ],
   })
 }
+
+
+# --- DynamoDB module ---
+module "dynamodb_table" {
+  source = "../../modules/dynamodb"
+
+  # meta argument 'depends_on' used outside module
+  depends_on = [module.vpc]
+
+  table_name = "${local.base_name}-table"
+  
+  # Same configuration as deprecated module
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+  
+  # Attribute definitions (same as deprecated)
+  attributes = [
+    {
+      name = "PK"
+      type = "S"
+    },
+    {
+      name = "SK"
+      type = "S"
+    },
+    {
+      name = "GSI1PK"
+      type = "S"
+    },
+    {
+      name = "GSI1SK"
+      type = "S"
+    }
+  ]
+  
+  # Global Secondary Index (same as deprecated)
+  global_secondary_indexes = [
+    {
+      name            = "GSI1"
+      hash_key        = "GSI1PK"
+      range_key       = "GSI1SK"
+      projection_type = "ALL"
+      read_capacity   = null
+      write_capacity  = null
+    }
+  ]
+  
+  # Enhanced features (new capabilities)
+  encryption_enabled              = true
+  point_in_time_recovery_enabled = true
+  deletion_protection_enabled    = false  # Set to true for production
+  
+  tags = local.common_tags
+}
